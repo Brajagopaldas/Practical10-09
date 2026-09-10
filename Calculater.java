a6574ad0b5f06c18a04baf21e1d5d119dacc66f4
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-10 18:56:44 +0530

    Add basic calculator functionality in Calculater.java

---
+++ Calculater.java
@@ -0,0 +1,21 @@
+class Calculater {
+	public static void main(String[] args){
+	int num1 = 45;
+	int num2 = 31;
+	
+	int sum = num1 + num2;
+	int difference = num1 - num2;
+	int product = num1 * num2;
+	double quotient = num1 / num2;
+	int reminder = num1 % num2;
+	
+	System.out.println("Sum = " + sum);
+	System.out.println("Difference = " + difference);
+	System.out.println("Product = " + product);
+	System.out.println("Quotient = " + quotient);
+	System.out.println("Reminder = " + reminder);
+
+	
+
+}
+}
