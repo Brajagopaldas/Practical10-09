9eadb9fd5d37ad3917112265972149dec0017521
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-11 13:13:54 +0530

    Add HraDaCalutation class to calculate salary components

---
+++ HraDaCalutation.java
@@ -0,0 +1,12 @@
+class HraDaCalutation {
+    public static void main(String[] args) {
+        
+        double salary = 75000;
+        double HRA = salary * 0.1;
+        double DA = salary * 0.2;
+        
+        System.out.println("Salary: " + salary);
+        System.out.println("HRA: " + HRA);
+        System.out.println("DA: " + DA);
+    }
+}
