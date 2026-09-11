3e8f010207d9ce3f1e5a412903f1d304c4cf8756
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-11 14:10:30 +0530

    Add PowerCalulation class to compute powers

---
+++ PowerCalulation.java
@@ -0,0 +1,12 @@
+class PowerCalulation {
+    public static void main (String[] args){
+        int number = 12;
+        int square = number * number;
+        int cube =  number * number * number;
+
+        System.out.println("Number = " + number);
+        System.out.println("Square = " + square);
+        System.out.println("Cube = " + cube);
+
+    }
+}
